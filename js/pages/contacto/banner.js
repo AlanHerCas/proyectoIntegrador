5ac64a724c64280e93f157a03816150a0b5e21f7
Xally Romina Ibanez Miranda <miranda.xall170497@gmail.com>
2026-06-11 01:59:26 -0600

    agregando mailjs

--- js/pages/contacto/banner.js
+++ js/pages/contacto/banner.js
@@ -1,11 +1,14 @@
 export function renderBanner() {
     return `
-        <!-- Contacto - Sección: Banner (En desarrollo) -->
-        <section class="container my-5 py-4 text-center border-bottom">
-            <h2 class="subtitulo" style="font-size: 1.8rem; font-weight: 700; color: var(--azul-rey-profundo);">
-                Contáctanos - Banner (En Desarrollo)
-            </h2>
-            <p class="texto-regular text-muted">Aquí se mostrará el banner de la página de Contacto.</p>
-        </section>
+      <section class="hero-banner">
+        <div class="hero-container">
+            <h2 class="hero-subtitle">Contáctanos</h2>
+            <h1 class="hero-title">Estamos aquì<br>para ayudarte</h1>
+            <p class="hero-text">
+                ¿Tienes dudas, sugerencias o necesitas ayuda? Nuestro equipo estará feliz de atenderte.
+            </p>
+        </div>
+    </section>
+
     `;
 }
